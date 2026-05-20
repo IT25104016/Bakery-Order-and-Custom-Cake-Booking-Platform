148e55dc834dbcde26d0a7eadf2ca246509a9912
IT25102144 <it25102144@my.sliit.lk>
2026-05-20 23:35:46 +0530

    Component 01 User & Authentication Management

--- src/main/java/com/bakery/model/PasswordResetToken.java
+++ src/main/java/com/bakery/model/PasswordResetToken.java
@@ -1,27 +1,29 @@
 package com.bakery.model;
+//E
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Data; //Generates getters/setters automatically.
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import java.time.LocalDateTime;//built in java class to store and manipulate date and time values without timezone information.
 
 @Entity
 @Table(name = "password_reset_tokens")
-@Data
+@Data //Generates getters/setters automatically.
 @NoArgsConstructor
 public class PasswordResetToken {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Database auto-generates ID
     private int id;
 
     @Column(nullable = false, unique = true, length = 100)
     private String token;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    //many password reset tokens can belong to one user
+    @ManyToOne(fetch = FetchType.LAZY) //User object is loaded only when needed.
     @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    private User user; //used association (password reset token associated with user)
 
     @Column(name = "expires_at", nullable = false)
     private LocalDateTime expiresAt;
