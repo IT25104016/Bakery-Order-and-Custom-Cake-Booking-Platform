5edbc21e6ff9d60428db71d941e5dc1fd72583d5
IT25104016 <it25104016@my.sliit.lk>
2026-05-20 22:49:55 +0530

    Refactor comments in DataInitializer class
    Updated comments for clarity and understanding of the code.

--- src/main/java/com/bakery/config/DataInitializer.java
+++ src/main/java/com/bakery/config/DataInitializer.java
@@ -2,27 +2,27 @@ package com.bakery.config;
 
 import com.bakery.model.*;
 import com.bakery.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.boot.CommandLineRunner;//Application start automatic initialization tasks run 
+import org.springframework.jdbc.core.JdbcTemplate;////jdbc use for exectute direct SQL queries
+import org.springframework.security.crypto.password.PasswordEncoder;//use for Password encrypt/hash 
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 
 @Component
-public class DataInitializer implements CommandLineRunner {
-
-    private final UserRepository userRepository;
+public class DataInitializer implements CommandLineRunner {//(abstraction).
+    //Dependency Injection Variables
+    private final UserRepository userRepository;//encapsulation
     private final PasswordEncoder passwordEncoder;
     private final JdbcTemplate jdbcTemplate;
 
     public DataInitializer(UserRepository userRepository,
-                           PasswordEncoder passwordEncoder,
+                           PasswordEncoder passwordEncoder,//hashing password
                            JdbcTemplate jdbcTemplate) {
         this.userRepository  = userRepository;
         this.passwordEncoder = passwordEncoder;
-        this.jdbcTemplate    = jdbcTemplate;
+        this.jdbcTemplate    = jdbcTemplate;//execute sql
     }
 
     @Override
@@ -53,10 +53,11 @@ public class DataInitializer implements CommandLineRunner {
 
             // ── Polymorphism demo in logs ──────────────────────
             System.out.println(" " + user.getDisplayInfo()
-                    + " → dashboard: " + user.getDashboardUrl());
+                    + " → dashboard: " + user.getDashboardUrl());//“A method gives different outputs according to the object type.”
+
         }
     }
-
+    //jdbc use for exectute direct SQL queries
     private void allowDeletedUsersInOrderHistory() {
         jdbcTemplate.execute("ALTER TABLE orders MODIFY COLUMN user_id INT NULL");
         jdbcTemplate.execute("ALTER TABLE custom_orders MODIFY COLUMN user_id INT NULL");
